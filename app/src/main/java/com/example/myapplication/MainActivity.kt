39a35866e0584f85c8c1509c41b048cbd961b792
APinCan <6794ch@naver.com>
2019-04-30 15:15:14 +0900

    호무새 적용

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    //g호우호우호우호우!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
